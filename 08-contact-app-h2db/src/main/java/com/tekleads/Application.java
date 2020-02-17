2905d2a25167e0e8c25501983966af3a775a5e94
rnp01 <rnpanda07@gmail.com>
2020-02-17 18:18:17 +0530

    sop added

--- 08-contact-app-h2db/src/main/java/com/tekleads/Application.java
+++ 08-contact-app-h2db/src/main/java/com/tekleads/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext appCtxt = SpringApplication.run(Application.class, args);
-		System.out.println("");
+		System.out.println("hello");
 	}
 
 }
