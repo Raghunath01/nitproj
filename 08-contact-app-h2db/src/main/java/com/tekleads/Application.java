211221887d49f1557fafe3088d91f5d8aef37f10
rnp01 <rnpanda07@gmail.com>
2020-02-17 15:56:43 +0530

    created ConfigurableAppContext in Appln.java

--- 08-contact-app-h2db/src/main/java/com/tekleads/Application.java
+++ 08-contact-app-h2db/src/main/java/com/tekleads/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext appCtxt = SpringApplication.run(Application.class, args);
-		appCtxt.getBean("");
+		
 	}
 
 }
